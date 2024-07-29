bc85cd6138d78546c71c0dbe00bcd4e4612c040f
Williams Parre <williamsparre@gmail.com>
2024-07-29 18:21:45 +0530

    Improving package management and performance management

--- init.el
+++ init.el
@@ -4,22 +4,4 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(nerd-icons-dired flyspell-correct-popup flyspell-correct yasnippet smartparens company-box company lsp-treemacs treemacs-all-the-icons lsp-ui lsp-mode flycheck format-all toggle-term magit rainbow-delimiters indent-guide toc-org org-bullets doom-modeline general consult orderless marginalia vertico which-key evil-commentary evil-goggles evil-indent-textobject evil-surround evil-collection evil ef-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-goggles-change-face ((t (:inherit diff-removed))))
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
+
